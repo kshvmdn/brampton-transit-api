5b06834af5756d065749cba0c6bb69174684574a
Kashav Madan <kshvmdn@gmail.com>
2015-12-30 02:39:16 -0500

    Log python output with child process (God Bless exec👌 l 0 l)

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,16 @@ var exec = require('child_process').exec;
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json(req);
+	var stop = parseInt(req.query.stop);
+	if (stop == null || !stop || isNaN(stop) ) {
+		res.json({ 'status': 400, 'message': 'Invalid stop code.'});
+		return;
+	}
+	exec('python3 main.py ' + stop, {cwd: '../scraper'}, function(error, stdout, stderr) {
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		}
+	});
 });
 
 module.exports = router;
